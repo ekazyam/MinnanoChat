2092034a9300552439eb1793fab7d8766747a361
ekazyam <readsnow77@gmail.com>
2015-12-18 16:48:18 +0900

    コメント追加

--- js/mmenu.js
+++ js/mmenu.js
@@ -5,7 +5,11 @@ $(document).ready(function() {
 jQuery(function($){
 	$('#links li a').click(function() {
 		var href = $(this).attr('href');
+
+		// index.htmlのobject要素にURLを読み込む
 		$('#content').load(href);
+
+		// 遷移したURLを保持
 		url_store(href);
 		return false;
 	});  
