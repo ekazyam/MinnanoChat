9a62b4f093b101d5bd843b1e7101b9b1520420e3
ekazyam <readsnow77@gmail.com>
2015-12-21 00:00:27 +0900

    アプリケーション制御系を別jsに分割

--- js/mmenu.js
+++ js/mmenu.js
@@ -6,16 +6,3 @@ $(document).ready(function(e) {
 	// ブックマークが存在した場合にロード。
 	read_bookmark();
 });
-
-jQuery(function($){
-	$('#links li a').click(function() {
-		var href = $(this).attr('href');
-
-		// index.htmlのobject要素にURLを読み込む
-		$('#content').load(href);
-
-		// 遷移したURLを保持
-		url_store(href);
-		return false;
-	});
-});
