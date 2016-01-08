90f72c654aa124b63efaf1d4c4ba43a258519d0d
ekazyam <readsnow77@gmail.com>
2016-01-08 23:02:56 +0900

    画面ポジションが正常にセーブ/ロードされていなかった問題を修正

--- main.js
+++ main.js
@@ -40,18 +40,26 @@ app.on("ready", function() {
 
 	// 保存した画面サイズ/ポジションを取得する。
 	var window_info;
+	var x_posi;
+	var y_posi;
 	try
 	{
+		// ポジションをコンフィグファイルからパース
 	    window_info = JSON.parse(fs.readFileSync('mincha.conf', 'utf8'));
+	    x_posi = window_info["x"];
+	    y_posi = window_info["y"];
 	}
 	catch(e)
 	{
+		// ポジションをデフォルト値からパース
 	    window_info = JSON.parse('{"width":700,"height":512}');
+	    x_posi = size["width"] - window_info["width"];
+	    y_posi = size["height"] - window_info["height"];
 	}
 	mainWindow = new BrowserWindow({
 		// 画面の右下(タスクバー欄)をデフォルト表示欄として固定する。
-		"x":size["width"] - window_info["width"],
-		"y":size["height"] - window_info["height"],
+		"x": + x_posi,
+		"y": + y_posi,
 		"width":window_info["width"],
 		"height":window_info["height"],
 		"skip-taskbar": true,
@@ -136,8 +144,7 @@ app.on("ready", function() {
 	    mainWindow.on('close', function(e){
 	    	if(close_flag)
 	    	{
-	    		// 閉じる直前のサイズと座標を保持する。
-	    		fs.writeFileSync('mincha.conf', JSON.stringify(mainWindow.getBounds()));
+	    		save_config();
 	    		// タスクトレイから終了時にアプリケーションを終了する。
 	    		mainWindow.destroy();
 	    		mainWindow = null;
@@ -155,11 +162,28 @@ app.on("ready", function() {
             e.preventDefault();
             mainWindow.hide();
 	    });
+
+	    // ウインドウを移動時に保存する。
+	    mainWindow.on('move', function (e) {
+	    	save_config();
+	    });
+
+	    // ウインドウリサイズ時に保存する。
+	    mainWindow.on('resize', function (e) {
+	    	save_config();
+	    });
 	}
 
 	// メニューバー作成。
 	function create_menubar()
 	{
 		mainWindow.setMenu(null);
+	}
+
+	// ステータス保存。
+	function save_config()
+	{
+    		// 閉じる直前のサイズと座標を保持する。
+    		fs.writeFileSync('mincha.conf', JSON.stringify(mainWindow.getBounds()));
 	}
 });
